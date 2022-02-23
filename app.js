8b5b14cb5d5c720a46e088cb2471cf603df9116e
PatrikVahlen <patrik.vahlen@gmail.com>
2022-02-23 20:31:38 +0100

    add

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Test")
+})
+
 app.listen(PORT, () => {
     console.log(`Started Express server on port ${PORT}`);
 });
