db3124b91a812ca3940803da924deeda7dc64ee8
PatrikVahlen <patrik.vahlen@gmail.com>
2022-03-02 14:50:40 +0100

    add update

--- models/tweets.js
+++ models/tweets.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const tweetSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.ObjectId, ref: "User" },
-    content: { type: String, required: true },
+    content: { type: String, required: true, maxLength: 32 },
     date: { type: Date, default: Date.now }
 });
 
